8d35a65a8d4ac7d7a0feb8ed095e8aef2d3e4f1d
mica-1999 <micael1999work@gmail.com>
2025-04-03 16:09:46 +0100

    slight change

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
 };
 
 export default nextConfig;
